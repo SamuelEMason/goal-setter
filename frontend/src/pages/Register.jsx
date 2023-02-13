8cd301d8957be69efb4cbd71be2df99a7746b500
sam <subterranean.layer@gmail.com>
2023-02-13 15:35:36 -0500

    completed app for production

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 const Register = () => {
 	const [formData, setFormData] = useState({
@@ -11,16 +17,49 @@ const Register = () => {
 
 	const { name, email, password, password2 } = formData;
 
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+		if(isSuccess || user) {
+			navigate('/');
+		}
+
+		dispatch(reset());
+	}, [user, isError, isSuccess, message, navigate, dispatch]);
+
 	const onChange = (e) => {
 		setFormData((prevState) => ({
-			...prevState, 
+			...prevState,
 			[e.target.name]: e.target.value,
 		}));
 	};
 
 	const onSubmit = (e) => {
 		e.preventDefault();
+
+		if (password !== password2) {
+			toast.error('Passwords do not match');
+		} else {
+			const userData = {
+				name,
+				email,
+				password,
+			};
+			dispatch(register(userData));
+		}
 	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
 
 	return (
 		<>
@@ -77,7 +116,7 @@ const Register = () => {
 						/>
 					</div>
 					<div className="form-group">
-						<button type='submit' className='btn btn-block'>
+						<button type="submit" className="btn btn-block">
 							Submit
 						</button>
 					</div>
